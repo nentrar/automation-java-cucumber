598a5cf5f32f2234e22776788af75ab59812a0ba
nentrar <nentrar.mail@gmail.com>
2019-10-08 16:31:22 +0200

    Add more cucumber statement methods

--- src/test/java/stepdefs/pages/r19/niam/WelcomePageDefinitions.java
+++ src/test/java/stepdefs/pages/r19/niam/WelcomePageDefinitions.java
@@ -1,5 +1,6 @@
 package stepdefs.pages.r19.niam;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.WebDriver;
 import pages.r19.netguard.welcomePage.WelcomePage;
@@ -12,12 +13,32 @@ public class WelcomePageDefinitions {
     private final int TIMEOUT_10SECONDS = 10;
 
     @When("^Resource Access Manager is open$")
-    public void user_enter_login_credentials(String username, String password) {
+    public void ram_is_open() {
 
         WelcomePage welcomePage = new WelcomePage(driver);
 
         welcomePage.openMenuStart();
         welcomePage.openResourceAccessManager();
+
+    }
+
+    @And("^Network is open$")
+    public void network_is_open() {
+
+        WelcomePage welcomePage = new WelcomePage(driver);
+
+        welcomePage.openMenuStart();
+        welcomePage.openNetwork();
+
+    }
+
+    @And("^Network Snapshot Browser is open$")
+    public void network_snapshot_browser_is_open() {
+
+        WelcomePage welcomePage = new WelcomePage(driver);
+
+        welcomePage.openMenuStart();
+
 
     }
 
