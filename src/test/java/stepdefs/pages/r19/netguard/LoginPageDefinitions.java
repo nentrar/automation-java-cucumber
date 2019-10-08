e4148704f71db15d960070ea19d37cc856d8d309
nentrar <nentrar.mail@gmail.com>
2019-10-08 16:30:20 +0200

    Add cucumber statement method for log in

--- src/test/java/stepdefs/pages/r19/netguard/LoginPageDefinitions.java
+++ src/test/java/stepdefs/pages/r19/netguard/LoginPageDefinitions.java
@@ -28,11 +28,24 @@ public class LoginPageDefinitions {
 //    public LoginPageDefinitions() throws FileNotFoundException {}
 
     @Given("^user open NIAM login page$")
-    public void login_page_is_open(String niamUrl) {
+    public void niam_login_page_is_open(String niamUrl) {
 
         System.setProperty("webdriver.chrome.driver", "driver/windows/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("http://192.168.56.1:8080/netguard_iam/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(TIMEOUT_10SECONDS, TimeUnit.SECONDS);
+
+
+
+    }
+
+    @Given("^user open NACM login page$")
+    public void nacm_login_page_is_open(String niamUrl) {
+
+        System.setProperty("webdriver.chrome.driver", "driver/windows/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://192.168.56.1:8080/netguard_acm/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(TIMEOUT_10SECONDS, TimeUnit.SECONDS);
 
@@ -60,6 +73,25 @@ public class LoginPageDefinitions {
 //        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 //        FileUtils.copyFile(scrFile, new File("testResults/SeleniumEasy/open_seleniumeasy_webpage_screenshot.png"));
 
+    }
+
+    @Given("^user log into NACM application with login \\\"([^\\\"]*)\\\" and password \\\"([^\\\"]*)\\\"$")
+    public void user_log_to_nacm(String login, String password) {
+
+        System.setProperty("webdriver.chrome.driver", "driver/windows/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://192.168.56.1:8080/netguard_iam/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(TIMEOUT_10SECONDS, TimeUnit.SECONDS);
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.enterUsername(login);
+        loginPage.enterPassword(password);
+        loginPage.clickEnterButton();
+
+        WelcomePage niamDashboard = new WelcomePage(driver);
+        Assert.assertTrue(niamDashboard.getLoggedUser().contains(login));
 
 
     }
