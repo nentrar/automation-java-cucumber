c59d44c15737dada2e8c18074b80ab4c9fbea151
nentrar <nentrar.mail@gmail.com>
2019-10-10 15:16:57 +0200

    Prepare cucumber statement methods for Gold Standard Page

---
+++ src/test/java/stepdefs/pages/r19/nacm/GoldStandardsDefinitions.java
@@ -0,0 +1,44 @@
+package stepdefs.pages.r19.nacm;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class GoldStandardsDefinitions {
+
+    WebDriver driver;
+
+    @Then("^Gold Standard is assigned to Network Element$")
+    public void gold_standard_is_assigned() {
+
+    }
+
+    @When("^Gold Standard is selected$")
+    public void gold_standard_is_selected() {
+
+    }
+
+    @And("^assigning gold standard operation is confirmed$")
+    public void gold_standard_confirmed() {
+
+    }
+
+    @And("^audit discrepancies should be shown$")
+    public void audit_discrepancies_shown() {
+
+    }
+
+    @Then("^automatic reconcile should be executed$")
+    public void automatic_reconcile_is_executed() {
+
+    }
+
+    @And("^anomaly should be corrected$")
+    public void anomaly_is_corrected() {
+
+    }
+
+
+
+}
