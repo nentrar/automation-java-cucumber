936706f690f25b184f81a3f2e83d6546cdc029ff
nentrar <nentrar.mail@gmail.com>
2019-10-10 15:17:15 +0200

    Add more methods

--- src/test/java/stepdefs/pages/r19/nacm/NetworkPageDefinitions.java
+++ src/test/java/stepdefs/pages/r19/nacm/NetworkPageDefinitions.java
@@ -1,6 +1,7 @@
 package stepdefs.pages.r19.nacm;
 
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import pages.r19.nacm.network.NetworkPage;
@@ -15,4 +16,26 @@ public class NetworkPageDefinitions {
         NetworkPage network = new NetworkPage(driver);
 
     }
+
+    @And("^Network Element Group \\\"([^\\\"]*)\\\" is selected$")
+    public void ne_group_is_selected(String groupName, String neName) {
+
+        NetworkPage network = new NetworkPage(driver);
+
+    }
+
+    @Then("^Gold Standard is created from network element$")
+    public void gold_standard_is_created() {
+
+
+    }
+
+    @And("^Gold Standard operation is confirmed$")
+    public void gold_standard_confirmed() {
+
+    }
+
+
+
+
 }
