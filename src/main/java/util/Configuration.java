8d1b33e6ee87236576fb5498bca59db60aae3478
nentrar <nentrar.mail@gmail.com>
2019-11-05 16:21:54 +0100

    Add Configuration class for read config.properties

---
+++ src/main/java/util/Configuration.java
@@ -0,0 +1,40 @@
+package util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Configuration {
+
+    String niamUrl;
+    String nacmUrl;
+
+    public Configuration() {
+
+        try (InputStream input = new FileInputStream("config/config.properties")) {
+
+            Properties prop = new Properties();
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value
+            this.niamUrl = prop.getProperty("niamUrl");
+            this.nacmUrl = prop.getProperty("nacmUrl");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public String getNiamUrl() {
+        return niamUrl;
+    }
+
+    public String getNacmUrl() {
+        return nacmUrl;
+    }
+}
