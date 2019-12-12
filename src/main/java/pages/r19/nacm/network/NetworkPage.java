4ae5a771ca8e8b5bcf91398e15a355c7ef708791
nentrar <nentrar.mail@gmail.com>
2019-12-12 16:04:42 +0100

    Small changes due to step definitions

--- src/main/java/pages/r19/nacm/network/NetworkPage.java
+++ src/main/java/pages/r19/nacm/network/NetworkPage.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class NetworkPage {
 
@@ -65,9 +67,18 @@ public class NetworkPage {
     @FindBy(xpath = "//button[text()='Save' and ancestor::td[following-sibling::td[descendant::button[contains(text(),'Revert')]]]]")
     WebElement GS_SAVE_BUTTON;
 
+    @FindBy(xpath = "//span[text()='Getting data snapshot from the Network Element...']")
+    WebElement GS_CREATE_IN_PROCESS;
+
+    @FindBy(xpath = "//span[text()='Audit in process...']")
+    WebElement AUDIT_IN_PROCESS;
+
+
     final String NETWORK_ELEMENT = "//span[contains(text(),'%s')]";
     final String NETWORK_SNAPSHOTS_NE = "//span[text()='Network Snapshots - %s']";
     final String GOLD_STANDARD_CREATED_WINDOW_XPATH = "//span[contains(text(),'Create Gold Standard Dataset from Network Element - %s')]";
+    final By AUDIT_COMPLETED = By.xpath("//span[@qtip='Audit Completed']");
+    final By NO_DISCREPANCIES = By.xpath("(//div[contains(@class,'x-grid3-col-totalNumberOfDiscrepancies') and child::span[contains(text(),'0')]])[2]");
 
     public NetworkPage(WebDriver driver) {
         this.driver = driver;
@@ -147,9 +158,19 @@ public class NetworkPage {
         USE_ASSIGNED_PROFILE.click();
     }
 
-    public void verifyAuditCompleted() {
+    public Boolean verifyAuditCompleted() throws InterruptedException {
 
-        // dopisać sensowną metodę sprawdzenia czy wykonał się audyt dająca true albo false pod assert
+        Thread.sleep(60000);
+
+        Boolean auditDone = driver.findElements(AUDIT_COMPLETED).size() > 0;
+
+        if (auditDone) {
+            return true;
+        } else {
+            return false;
+        }
+
+
     }
 
     public void createGoldStandard(String neName, String gsName) {
@@ -160,6 +181,46 @@ public class NetworkPage {
         GS_SAVE_BUTTON.click();
 
         // dopisać jakaś metodą sprawdzającą, że GS się rzeczywiście utworzył
+    }
+
+    public void verifyGoldStandardCreation() throws InterruptedException {
+
+//        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT_5MINUTES);
+//        wait.until(ExpectedConditions.invisibilityOf(GS_CREATE_IN_PROCESS));
+        Thread.sleep(180000);
+        SAVE_BUTTON.click();
+
+
+    }
+
+    public Boolean auditShowDiscrepancies(WebDriver driver) throws InterruptedException {
+
+        Thread.sleep(120000);
+
+        Boolean auditDone = this.driver.findElements(NO_DISCREPANCIES).size() < 0;
+
+        if (auditDone) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+    public Boolean auditShowNoDiscrepancies() throws InterruptedException{
+
+        Thread.sleep(120000);
+
+        Boolean auditDone = driver.findElements(NO_DISCREPANCIES).size() > 0;
+
+        if (auditDone) {
+            return true;
+        } else {
+            return false;
+        }
+
+
     }
 
 
